5ef3d8d883949966b6dd6a6fa2fda97a3d330d9b
Neil-Jungayo <neil.jungayo@acescredit.ph>
2024-11-15 15:14:35 +0800

    Added ApplicationDbContext class Added AddDbContext to services Added options for the AddDbContext where it get the DefaultConnection

---
+++ HRISClient/Services/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HRISClient.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
